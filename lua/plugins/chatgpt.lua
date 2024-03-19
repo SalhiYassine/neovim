a7673bb3285c0330f2bb4da57e0994a178ace6a0
Yassine Salhi <yassinesalhi135@gmail.com>
2024-03-19 10:07:25 +0000

    add keybind for chatgpt

--- lua/plugins/chatgpt.lua
+++ lua/plugins/chatgpt.lua
@@ -5,6 +5,8 @@ return { -- Lazy
 		config = function()
 			require("chatgpt").setup()
 			local chatgpt = require("chatgpt")
+
+			vim.api.nvim_set_keymap("n", "<leader>cc", "<cmd>ChatGPT<CR>", { noremap = true, silent = true })
 		end,
 		dependencies = {
 			"MunifTanjim/nui.nvim",
